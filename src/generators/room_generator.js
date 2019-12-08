305c541d72cb822200dc5b0ab118998a319dbcbf
Brenden <bsatake@ucsc.edu>
2019-12-07 18:46:57 -0800

    Added the partition_rooms function

--- src/generators/room_generator.js
+++ src/generators/room_generator.js
@@ -122,3 +122,29 @@ function create_tunnel (array, xOrigin, yOrigin, xroom, yroom){
     
     create_room(array,xtunnel,ytunnel,xwidth,yheight);
 }
+
+function partition_rooms(X0,Y0,X,Y,ROOMS){
+    var partitions = [];
+    if(ROOMS == 0){
+        // Probably not possible
+        return partitions;
+    }
+    if(ROOMS == 1){
+        partitions.push([X0,Y0,X,Y]);
+        return partitions;
+    }
+    var part1 = Math.floor(ROOMS/2);
+    var part2 = ROOMS - part1; 
+    if(ROOMS % 2 == 0){
+        var split = Math.floor(X/2);
+        partitions.push(partition_rooms(X0,Y0,split,Y,part1));
+        partitions.push(partition_rooms(split,Y0,X,Y,part2));
+        return(partitions);
+    }
+    if(ROOMS % 2 == 1){
+        var split = Math.floor(Y/2);
+        partitions.push(partition_rooms(X0,Y0,X,split,part1));
+        partitions.push(partition_rooms(X,split,X,Y,part2));
+        return(partitions);
+    }
+}
