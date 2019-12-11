bb65690bbc8561cf4946b0500ebc89e804481cde
Seiji Emery <seijiemery@gmail.com>
2019-12-11 01:54:28 -0800

    texture rendering

--- cppsrc/systems/renderer_system.hpp
+++ cppsrc/systems/renderer_system.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "../utils/all_includes.hpp"
+#include "../utils/load_png.hpp"
+#include <unordered_map>
 
 #ifdef __APPLE__
 #include <OpenGL/gl3.h>
@@ -9,21 +11,32 @@
 DEFINE_EXCEPTION(GLRuntimeException)
 DEFINE_EXCEPTION(ShaderCompileException)
 DEFINE_EXCEPTION(ShaderLinkException)
+DEFINE_EXCEPTION(TextureLoadException)
 
 static const char* VERTEX_SHADER_SRC = R"SHADER(
 #version 410
 in vec2 vertexPos;
+out vec2 texCoord;
+uniform float width;
+uniform float height;
 
 void main () {
-    gl_Position = vec4(vertexPos, 0.0, 1.0);
+    texCoord = vertexPos + vec2(0.5, 0.5);
+    gl_Position = vec4(
+        vertexPos.x * width / 800,
+        vertexPos.y * height / 600, 
+        0.0, 1.0);
 }
 )SHADER";
 
 static const char* FRAGMENT_SHADER_SRC = R"SHADER(
 #version 410
 out vec4 fragColor;
+in vec2 texCoord;
+uniform sampler2D tex;
 void main () {
-    fragColor = vec4(1, 1, 1, 1);
+    fragColor = texture(tex, texCoord);
+    fragColor.rgb *= fragColor.a;
 }
 )SHADER";
 
@@ -54,6 +67,7 @@ static void _glCheckErrors(const char* info, const char* file, int line) {
 
 class ShaderProgram {
     GLuint program = 0;
+    std::unordered_map<std::string, GLuint> uniformLocations;
 private:
     static GLuint createAndCompileShader (const char* shaderSrc, GLenum shaderType) {
         GLuint shader = glCreateShader(shaderType);
@@ -83,6 +97,7 @@ private:
         return shader;
     }
 public:
+    GLuint get () { assert(program); return program; }
     void load (const char* vertex, const char* fragment) {
         GLuint vertexShader = createAndCompileShader(vertex, GL_VERTEX_SHADER);
         GLuint fragmentShader = createAndCompileShader(fragment, GL_FRAGMENT_SHADER);
@@ -120,6 +135,26 @@ public:
     void bind () {
         glUseProgram(program);
     }
+    template <typename T>
+    void setUniform (const std::string& name, const T& value) {
+        auto it = uniformLocations.find(name);
+        if (it != uniformLocations.end()) {
+            setUniform(it->second, value);
+        } else {
+            auto uniformLocation = glGetUniformLocation(program, name.c_str());
+            uniformLocations[name] = uniformLocation;
+            setUniform(uniformLocation, value);
+        }
+    }
+private:
+    #define DEFN_UNIFORM_SET1V(Type, fcn) \
+        void setUniform(GLuint loc, Type value) { \
+            fcn(loc, value); glCheckErrors(#fcn); \
+        }
+    DEFN_UNIFORM_SET1V(int, glUniform1i)
+    DEFN_UNIFORM_SET1V(float, glUniform1f)
+
+    #undef DEFN_UNIFORM_SET1V
 };
 
 class VBO {
@@ -187,19 +222,59 @@ public:
     }
 };
 
+class Texture {
+    GLuint tex = 0;
+    int width, height;
+public:
+    void load (const std::string& path) {
+        if (!tex) {
+            glGenTextures(1, &tex); glCheckErrors("glGenTextures");
+        }
+        glBindTexture(GL_TEXTURE_2D, tex); glCheckErrors("glBindTexture");
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER); glCheckErrors("glTexParameteri");
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER); glCheckErrors("glTexParameteri");
+        float color[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, color); glCheckErrors("glTexParameterfv");
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); glCheckErrors("glTexParameteri");
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); glCheckErrors("glTexParameteri");
+
+        unsigned char* imageData;
+        bool hasAlpha = true;
+        if (loadPngImage(path.c_str(), width, height, hasAlpha, &imageData)) {
+            printf("loaded png data!\n");
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                      GL_UNSIGNED_BYTE, imageData); glCheckErrors("glTexImage2D");
+            glGenerateMipmap(GL_TEXTURE_2D); glCheckErrors("glGenerateMipmap");
+            free(imageData);
+        } else {
+            ENFORCE_WITH(TextureLoadException, false, "Could not load image '%s'", path.c_str());
+        }
+    }
+    void bind (ShaderProgram& program, const std::string& name, int location = 0) {
+        glActiveTexture(GL_TEXTURE0 + location); glCheckErrors("glActiveTexture");
+        glBindTexture(GL_TEXTURE_2D, tex); glCheckErrors("glBindTexture");
+        program.setUniform(name, location);
+        program.setUniform("width", (float)width);
+        program.setUniform("height", (float)height);
+    }
+};
+
 struct RendererSystem {
     ShaderProgram program;
+    Texture texture;
     QuadMesh quad;
 
     RendererSystem () {
         printf("running on opengl %s\n", glGetString(GL_VERSION));
         program.load(VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC);
         quad.load();
+        texture.load("assets/assets_kenny_dungeon_pack/Isometric/barrel_E.png");
         printf("init renderer!\n");
     }
     void update () {
         printf("update renderer!\n");
         program.bind();
+        texture.bind(program, "texture", 0);
         quad.bind();
         quad.draw();
     }
