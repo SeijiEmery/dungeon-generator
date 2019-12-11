2902bef195de771c2f015878a5ab7fdcb7d07f93
Seiji Emery <seijiemery@gmail.com>
2019-12-10 19:48:02 -0800

    got opengl working

--- cppsrc/main.cpp
+++ cppsrc/main.cpp
@@ -27,24 +27,41 @@ std::string format(const char* fmt, ...) {
 #define THROW(...) throw std::runtime_error(format(__VA_ARGS__))
 
 int main(int argc, char *argv[]) {
-    ENFORCE_CRITICAL(
-        glfwInit(), 
-        "Failed to initialize glfw!");
-
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize glfw!\n");
+        exit(-1);
+    }
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
     GLFWwindow* window = glfwCreateWindow(
         WINDOW_WIDTH, WINDOW_HEIGHT,
         "dungeon-generator", NULL, NULL);
+    if (window == nullptr) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        exit(-1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     ENFORCE_CRITICAL(window != nullptr,
         "Failed to create GLFW window");
 
     try {
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
         THROW("fubar %s %d %0.2f", "bar", 10, 0.25);
-
         throw std::runtime_error("fubar");
     } catch (const std::exception& e) {
         fprintf(stderr, "terminated with error:\n\t%s\n", e.what());
     }
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
